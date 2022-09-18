7be9563b0d8e2169ed5e00002cab973cad4a254a
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-09-18 09:35:56 -0500

    initial commit

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -10,7 +9,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,7 +111,7 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
@@ -120,6 +119,18 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        //System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.print("No Results");
+        }
+
+        String border = "*****";
+
+        someJobs.forEach(job -> {
+            System.out.println();
+            System.out.println(border);
+            job.forEach((jobKey, jobValue) -> System.out.printf("%s: %s%n", jobKey, jobValue));
+            System.out.println(border);
+        });
     }
 }
